e88c37d12ea17f0ba50a670daff0ed70637ac3d3
Skadisdog <3361286723@qq.com>
2024-10-02 13:46:54 +0800

    创建fitting模块

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ using namespace cv;
 int main()
 {
     std::chrono::milliseconds t = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch());
-    WINDMILL::WindMill wm(t.count());
+    WINDMILL::WindMill wm(t.count()/1000);
     TARGET::Target tg;
     cv::Mat src;
 
@@ -20,5 +20,9 @@ int main()
         tg.findTarget(src);
         cv::Point2f RCenter = tg.getR();
         cv::Point2f targetCenter = tg.getTarget();
+        /*circle(src, targetCenter, 3, Scalar(255,0,0), -1);
+        cv::imshow("1", src);
+        cv::waitKey(1);*/
     }
+    return 0;
 }
